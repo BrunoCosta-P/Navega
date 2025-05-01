6792c00c95ef74e8ff139475a441b69a59c09ec9
Bruno Costa <brunocosta524@hotmail.com>
2025-05-01 17:08:53 -0300

    feat: declarando novos compoentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FormLoginComponent } from './modules/form-login/form-login.component';
+import { BackgroundLoginComponent } from './modules/background-login/background-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    FormLoginComponent,
+    BackgroundLoginComponent
   ],
   imports: [
     BrowserModule,
