de4c6c4aac209052e550549afe5ea1f248084316
Bruno Costa <brunocosta524@hotmail.com>
2025-05-01 17:08:10 -0300

    chore: alterando nomenclatura dos arquivos

---
+++ src/app/modules/background-login/background-login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-background-login',
+  templateUrl: './background-login.component.html',
+  styleUrls: ['./background-login.component.less']
+})
+export class BackgroundLoginComponent {
+
+}
