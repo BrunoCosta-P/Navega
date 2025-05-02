a48dc83bfa4320ec0b93c68f79a5c1c82ccf0a75
Bruno Costa <brunocosta524@hotmail.com>
2025-05-02 00:24:56 -0300

    feat: ajustando componentes do login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -14,11 +14,12 @@ import { BackgroundLoginComponent } from './modules/background-login/background-
     AppComponent,
     LoginComponent,
     HomeComponent,
-    FormLoginComponent,
     BackgroundLoginComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
+    FormLoginComponent,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
--- src/app/modules/form-login/form-login.component.ts
+++ src/app/modules/form-login/form-login.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  signal,
+  OnInit
+} from '@angular/core';
+import {
+  FormControl,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { merge } from 'rxjs';
 
 @Component({
   selector: 'app-form-login',
+  standalone: true,
   templateUrl: './form-login.component.html',
-  styleUrls: ['./form-login.component.less']
+  styleUrls: ['./form-login.component.less'],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FormLoginComponent {
+export class FormLoginComponent implements OnInit {
+  readonly email = new FormControl('', [Validators.required, Validators.email]);
+  version: string = '';
+  errorMessage = signal('');
+  hide = signal(true);
 
+  constructor() {
+    merge(this.email.statusChanges, this.email.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorMessage());
+  }
+
+  ngOnInit(): void {
+    this.version = '0.1.0';
+  }
+
+  updateErrorMessage() {
+    if (this.email.hasError('required')) {
+      this.errorMessage.set('You must enter a value');
+    } else if (this.email.hasError('email')) {
+      this.errorMessage.set('Not a valid email');
+    } else {
+      this.errorMessage.set('');
+    }
+  }
+
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
 }
