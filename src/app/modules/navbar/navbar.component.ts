b7cc005e3c43f8e74c0a17b3393e85c059553ea8
Bruno Costa <brunocosta524@hotmail.com>
2025-05-03 12:51:35 -0300

    feat: criando logica de responsividade

--- src/app/modules/navbar/navbar.component.ts
+++ src/app/modules/navbar/navbar.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.less']
+  styleUrls: ['./navbar.component.less'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  isMobile: boolean = false;
+  mobileBreakpoint: number = 768; // Defina o breakpoint que você considera "mobile"
 
+  constructor() {
+    this.checkScreenWidth(); // Verifica a largura inicial da tela
+  }
+
+  ngOnInit(): void {
+    this.checkScreenWidth(); // Garante que a verificação seja feita na inicialização
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkScreenWidth(); // Verifica a largura da tela sempre que a janela é redimensionada
+  }
+
+  checkScreenWidth(): void {
+    this.isMobile = window.innerWidth < this.mobileBreakpoint;
+  }
 }
